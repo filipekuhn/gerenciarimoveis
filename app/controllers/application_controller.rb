e38ff7d082c76076e512f8aabe85e39caa23b125
Filipe Kuhn <filipe_kuhn@hotmail.com>
2017-11-09 20:36:29 -0200

    Include user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
 
   protected
@@ -8,4 +10,9 @@ class ApplicationController < ActionController::Base
 
     "layouts/application"
   end
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+   end
 end
