694263c677227ef3913dd760d74874020d2fbe5b
Filipe Kuhn <filipe_kuhn@hotmail.com>
2017-11-13 22:18:24 -0200

    Application_controller fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,18 +27,14 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource == :admin
       admins_dashboard_index_path
-    else resource == :user
+    elsif resource == :user
       users_dashboard_index_path
+    else
+      root_path
     end
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :user
-      root_path
-    elsif resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
-    end
+    root_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @title = "Gerenciar Imóveis"
     @users_properties = Users::Property.all
   end
 
   def rent
-    @title = "Imóveis para Alugar"
+    @title = "Gerenciar Imóveis - Locação"
   	@users_properties = Users::Property.where(flag: "Alugar")
   end
 
